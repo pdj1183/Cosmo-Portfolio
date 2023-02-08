7aad8b1d08af54ee48427530e64f2501e9056011
Phillip Johnson <pdj1183@gmail.com>
2023-02-08 15:15:04 -0700

    Edited the sidebar and cover image

--- cosmo/src/components/CoverScreen.js
+++ cosmo/src/components/CoverScreen.js
@@ -1,11 +1,9 @@
 import "./CoverScreen.css";
-import coverImg from "../assets/img/moab22.jpg";
 
 export const CoverScreen = () => {
-  return (
-    <div className="coverScreen">
-      <img src={coverImg} className="coverScreenImg" alt="moab22" />
-      <h1>HEEELLLO</h1>
-    </div>
-  );
+    return (
+        <span>
+            <section className="coverScreen"></section>
+        </span>
+    );
 };
